35561e7039c75c64b18512337913ae2814af125d
eugenebondar0508 <larisa-bondar1@ukr.net>
2021-06-17 22:19:46 +0300

    lesson 17

--- script/script.js
+++ script/script.js
@@ -1,13 +1,44 @@
 'use strict';
- class First{
-     static hello() {
-         console.log('Привет я метод родителя!')
-     }
- }
-class Second extends First{
-   static hello(){
-      super.hello();  
-      console.log('А я наследуемый метод!');
-    }
-};
-Second.hello();
+
+
+let hours = new Date();
+let timesOfDay = function(){
+  if(hours.getHours() <= 6 || hours.getHours() >= 22){
+    alert('Доброй ночи!');
+  } else if(hours.getHours() < 18 && hours.getHours() > 10){
+    alert('Добрый день!');
+  } else if(hours.getHours() > 18 && hours.getHours() < 22 ){
+    alert('Добрыый вечер!');
+  } else {
+    alert('Доброе утро!');
+  }
+}
+timesOfDay();
+
+let days = [
+  'Воскресенье',
+  'Понедельник',
+  'Вторник',
+  'Среда',
+  'Четверг',
+  'Пятница',
+  'Суббота'
+];
+let d = new Date();
+let n = d.getDay();
+console.log(`Сегодня: ${days[n]}`);
+
+let date = new Date();
+console.log(`Текущуу время : ${date.toLocaleTimeString('en')}`);
+
+
+
+let NewYear = new Date(" Jan 1, 2022 00:00:00").getTime();
+let count = setInterval(function(){
+  let now = new Date().getTime();
+  let distance = NewYear - now;
+  let days = Math.floor(distance/(1000*60*60*24));
+  console.log(`До Нового Года осталось: ${days} Дней`)
+
+}, 1000)
+
